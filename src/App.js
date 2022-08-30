540f637cce906039bd1ef1e360aebf90fe52c728
Younes Krifa <younes.krifa@gmail.com>
2022-08-30 10:45:12 +0100

    comit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import {useState} from "react";
-import un from './un.png';
-import deux from './deux.png';
-import trois from './trois.png';
-import citron from './citron.png';
-import fraise from './fraise.png';
-import orange from './orange.png';
-import kiwi from './kiwi.png';
-import banane from './banane.png';
-import samir from './samir.png';
-import cassis from './cassis.png';
-import goutte from './goutte.png';
-import eau from './eau.png';
+import citron from './1.png';
+import fraise from './12.png';
+import orange from './123.png';
+import kiwi from './1234.png';
+import banane from './12345.png';
+import cassis from './123456.png';
 
 function App() {
     const [currentSaveur, setCurrentSaveur] = useState(null);
     const [durationLevel1, setDurationLevel1] = useState(2000);
     const [durationLevel2, setDurationLevel2] = useState(3000);
     const [durationLevel3, setDurationLevel3] = useState(4000);
+    const [showAll, setShowAll] = useState(false);
+
     const [showParam, setShowParam] = useState(false);
     const [durationEau, setDurationEau] = useState(10000);
     const [isLoading, setIsLoading] = useState(false);
     const [code, setCode] = useState(false);
     const saveurs = [1, 2, 3, 4, 5, 6];
-    const colors = ['#ffdb72', '#f44336', '#e9841e', '#45c71c', '#8bc34a', 'white'];
+    const colors = ['#ffdb72', '#f44336', '#e9841e', '#45c71c', '#8bc34a', '#3300cb'];
     const saveursPhoto = [citron, fraise, orange, kiwi, banane, cassis];
 
     const stopProgram = () => {
@@ -45,9 +41,7 @@ function App() {
         if (isLoading === false) {
             setIsLoading(true);
             let timems = durationLevel1;
-            if (saveur === 6) {
-                timems = durationEau;
-            } else if (level === 2) {
+            if (level === 2) {
                 timems = durationLevel2;
             } else if (level === 3) {
                 timems = durationLevel3;
@@ -56,12 +50,12 @@ function App() {
 
             fetch(url)
                 .then((response => {
-                    setIsLoading(false);
-                    setCurrentSaveur(null);
+                   setIsLoading(false);
+                   setCurrentSaveur(null);
                 }))
                 .catch((error) => {
-                    setIsLoading(false);
-                    setCurrentSaveur(null);
+                   setIsLoading(false);
+                   setCurrentSaveur(null);
                 })
         }
     }
@@ -69,112 +63,92 @@ function App() {
     return (
         <div className="App-header">
 
-                <div className="wrapper">
-                    {saveurs.map((saveur => {
-                        if (saveur === currentSaveur) {
-                            if (isLoading) {
-                                return <div className="item"
-                                            style={{boxShadow: '1px 2px 4px #4e4e4e', borderColor: colors[saveur - 1]}}>
-                                    <img src={logo} className="App-logo" alt="logo"/></div>
-                            }
-                            if (saveur === 6) {
-                                return <div className="item"
-                                            style={{boxShadow: '1px 2px 4px #4e4e4e', borderColor: colors[saveur - 1]}}>
-                                    <div style={{flexDirection: 'column'}}>
-                                        <input name={"code"} placeholder={"Entrer le code"}
-                                               onChange={(e) => setCode(e.target.value)}/>
-                                        <div onClick={() => {
-                                            if (code === '12340z') {
-                                                setCode(null);
-                                                startProgram(saveur, 1);
-                                            }
-                                        }} style={{alignItems: 'center', height: 68, display: 'flex'}}>VALIDER
-                                        </div>
-                                    </div>
-                                </div>;
-                            }
-                            return (<div className="item"
-                                         style={{boxShadow: '1px 2px 4px #4e4e4e', borderColor: colors[saveur - 1]}}>
-                                <div style={{flexDirection: 'column'}}>
-                                    <div onClick={() => startProgram(saveur, 1)} style={{
-                                        alignItems: 'center',
-                                        height: 66,
-                                        width: 200,
-                                        borderRadius: 15,
-                                        justifyContent: 'center',
-                                        display: 'flex',
-                                        color: 'WHITE'
-                                    }}>
-                                        <span style={{fontSize: 18, fontWeight: 'bold'}}>Faible</span></div>
-                                    <div onClick={() => startProgram(saveur, 2)} style={{
-                                        alignItems: 'center',
-                                        height: 66,
-                                        width: 200,
-                                        borderRadius: 15,
-                                        justifyContent: 'center',
-                                        display: 'flex',
-                                        color: 'WHITE'
+            <div style={{position: 'absolute', top: 75, color: '#03a9f4', fontWeight: 'bold'}}>
+                {currentSaveur === null && <>Sélectionner un parfum</>}
+                {currentSaveur !== null && <>Sélectionner une concentation</>}
 
-                                    }}>
-                                        <span style={{fontSize: 18, fontWeight: 'bold'}}>Moyenne</span>
-                                    </div>
-                                    <div onClick={() => startProgram(saveur, 3)} style={{
-                                        alignItems: 'center',
-                                        height: 66,
-                                        width: 200,
-                                        borderRadius: 15,
-                                        justifyContent: 'center',
-                                        display: 'flex',
-                                        color: 'WHITE'
-
-                                    }}>
-                                            <span style={{fontSize: 18}}>
-                                                <span style={{fontSize: 18, fontWeight: 'bold',}}>Forte</span>
-</span>
-
-                                    </div>
-                                </div>
-                            </div>);
+            </div>
+            <div className="wrapper">
+                {saveurs.map((saveur => {
+                    if (saveur === currentSaveur) {
+                        if (isLoading) {
+                            return <div style={{
+                                alignItems: 'center',
+                                width: '100%',
+                                justifyContent: 'center',
+                                display: 'flex',
+                            }} >
+                                <img src={logo} className="App-logo" alt="logo"/></div>
                         }
-                        return <div className="item" style={{
-                            boxShadow: '1px 2px 4px black',
-                            borderColor: colors[saveur - 1]
-                        }} onClick={() => {
-                            if (isLoading === false) {
-                                setCurrentSaveur(saveur);
-                            }
-                        }}>
-                            {<><img src={saveursPhoto[saveur - 1]} width={100}
-                                    height={saveur === 4 || saveur === 5 ? 70 : 100}/> </>}</div>
-                    }))}
-                </div>
 
-                <div onClick={() => stopProgram()} style={{
-                    marginTop: 10,
-                    alignItems: 'center',
-                    height: 50,
-                    width: 100,
-                    borderRadius: 5,
-                    display: 'flex',
-                    justifyContent: 'center',
-                    backgroundColor: 'rgb(255,0,89)'
-                }}>STOP
-                </div>
+                        return (<div>
+                            <div style={{flexDirection: 'column'}}>
+                                <div onClick={() => startProgram(saveur, 1)} style={{
+                                    alignItems: 'center',
+                                    height: 66,
+                                    width: '100%',
+                                    justifyContent: 'center',
+                                    display: 'flex',
+                                }}>
+                                    <span style={{color: 'black',fontSize: 18, fontWeight: 'bold'}}>Faible</span></div>
+                                <div onClick={() => startProgram(saveur, 2)} style={{
+                                    alignItems: 'center',
+                                    height: 66,
+                                    width: '100%',
+                                    justifyContent: 'center',
+                                    display: 'flex',
+                                }}>
+                                    <span style={{color: 'black',fontSize: 18, fontWeight: 'bold'}}>Moyenne</span>
+                                </div>
+                                <div onClick={() => startProgram(saveur, 3)} style={{
+                                    alignItems: 'center',
+                                    height: 66,
+                                    width: '100%',
+                                    justifyContent: 'center',
+                                    display: 'flex',
+                                }}>
+                                                <span style={{color: 'black',fontSize: 18, fontWeight: 'bold',}}>Forte</span>
 
-                <div style={{marginTop: 10}}>
-                    <span onClick={() => setShowParam(!showParam)}>Paramétres</span>
-                    {showParam && <>
-                        <div>Durée Niveau 1 <input name={"durationLevel1"} value={durationLevel1}
-                                                   onChange={(e) => setDurationLevel1(e.target.value)}/></div>
-                        <div>Durée Niveau 2 <input name={"durationLevel2"} value={durationLevel2}
-                                                   onChange={(e) => setDurationLevel2(e.target.value)}/></div>
-                        <div>Durée Niveau 3 <input name={"durationLevel3"} value={durationLevel3}
-                                                   onChange={(e) => setDurationLevel3(e.target.value)}/></div>
-                        <div>Durée Eau <input name={"durationnNettoyage"} value={durationEau}
-                                                    onChange={(e) => setDurationEau(e.target.value)}/></div>
-                    </>}
-                </div>
+                                </div>
+                            </div>
+                        </div>);
+                    }
+                    return <div style={{marginRight: 4}} onClick={() => {
+                        if (isLoading === false) {
+                            setCurrentSaveur(saveur);
+                        }}}> <img  src={saveursPhoto[saveur - 1]} width={'100%'}
+                                /></div>
+                }))}
+            </div>
 
+
+            {showAll && <>
+            <div onClick={() => stopProgram()} style={{
+                marginTop: 10,
+                alignItems: 'center',
+                height: 50,
+                width: 100,
+                borderRadius: 5,
+                display: 'flex',
+                justifyContent: 'center',
+                backgroundColor: 'rgb(255,0,89)'
+            }}>STOP
+            </div>
+
+            <div style={{marginTop: 10}}>
+                <span onClick={() => setShowParam(!showParam)}>Paramétres</span>
+                {showParam && <>
+                    <div>Durée Niveau 1 <input name={"durationLevel1"} value={durationLevel1}
+                                               onChange={(e) => setDurationLevel1(e.target.value)}/></div>
+                    <div>Durée Niveau 2 <input name={"durationLevel2"} value={durationLevel2}
+                                               onChange={(e) => setDurationLevel2(e.target.value)}/></div>
+                    <div>Durée Niveau 3 <input name={"durationLevel3"} value={durationLevel3}
+                                               onChange={(e) => setDurationLevel3(e.target.value)}/></div>
+                    <div>Durée Eau <input name={"durationnNettoyage"} value={durationEau}
+                                          onChange={(e) => setDurationEau(e.target.value)}/></div>
+                </>}
+            </div>
+           </>}
         </div>
     );
 }
